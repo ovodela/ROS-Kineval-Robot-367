12bde8ad179112d8e9c4953f664c2a219ab32649
ovodela <102893727+ovodela@users.noreply.github.com>
2023-01-12 15:51:33 -0500

    first additions

--- ROS/catkin_ws/src/fetch_controller/src/Fetch_Controller.cpp
+++ ROS/catkin_ws/src/fetch_controller/src/Fetch_Controller.cpp
@@ -5,8 +5,13 @@ Fetch_Controller::Fetch_Controller(ros::NodeHandle &nh)
     nh_ = nh;
 
     //TODO: initialize a subscriber that is set to the channel "/base_scan". Set its callback function to be Laser_Scan_Callback
-    //TODO: initialize a publisher that is set to the channel "/cmd_vel"
+    ros::Subscriber sub = nh_.subscribe("/base_scan", 1000, &Fetch_Controller::Laser_Scan_Callback, this);
 
+    //TODO: initialize a publisher that is set to the channel "/cmd_vel"
+    ros::Publisher pub = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
+
+
+    //ros::Rate loop_rate(msg_laser_scan.scan_time);
 
 }
 
@@ -19,5 +24,34 @@ void Fetch_Controller::Laser_Scan_Callback(const sensor_msgs::LaserScan::ConstPt
     If the minimum scan in this direction is greater than 1m, drive forward. 
     Otherwise, turn left. 
     */
+    ros::Publisher pub;
+    geometry_msgs::Twist twist; 
 
+    float mid = msg_laser_scan->angle_max - msg_laser_scan->angle_min;
+    float lower_bound = mid - 1.0472;
+    float upper_bound = mid + 1.0472; 
+
+    float min_range = 100;
+    for (; lower_bound < upper_bound; lower_bound += msg_laser_scan->angle_increment){
+        if (msg_laser_scan->ranges[lower_bound] < min_range)
+            min_range = msg_laser_scan->ranges[lower_bound];
+    }
+
+    if (min_range > 1){
+        //linear velocity .5m/s forward
+        twist.linear.x = .5;
+        twist.linear.y = 0;
+        twist.linear.z = 0;
+        twist.angular.x = twist.angular.y = twist.angular.z = 0;
+    }
+    else{
+        //turn left 1radian/s
+        twist.linear.x = twist.linear.y = twist.linear.z = 0;
+        twist.angular.x = 0;
+        twist.angular.y = -1;
+        twist.angular.z = 0;
+
+    }
+
+    pub.publish(twist);
 }
