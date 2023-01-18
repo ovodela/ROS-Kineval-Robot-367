f5b74a4bedef61bb419329bb35ce7838c7fe7b2b
ovodela <102893727+ovodela@users.noreply.github.com>
2023-01-18 14:03:20 -0500

    1/18 - 2pm

--- project_pathplan/graph_search.js
+++ project_pathplan/graph_search.js
@@ -23,8 +23,8 @@
 /||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\
 \/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/||\/*/
 
-function getElement(heap, index){
-    return heap[index - 1][2];
+function getElement(G, heap, index){
+    return G[heap[index - 1][0], heap[index - 1][1]].priority;
 }
 
 function swap(heap, index1, index2){
@@ -33,36 +33,35 @@ function swap(heap, index1, index2){
     heap[index2 - 1] = temp;
 }
 
-function shiftUp(heap){
+function shiftUp(G, heap){
     var index = heap.length;
-    
-    while (index > 1 && (getElement(heap, math.floor(index / 2)) > getElement(heap, index))){ // 2 > 1
-        swap(heap, index, math.floor(index / 2));
-        index = math.floor(index / 2);
+    while (index > 1 && (getElement(G, heap, Math.floor(index / 2)) > getElement(G, heap, index))){ // 2 > 1
+        swap(heap, index, Math.floor(index / 2));
+        index = Math.floor(index / 2);
     }
 }
 
-function shiftDown(heap, k){
+function shiftDown(G, heap, k){
     var sz = heap.length;
     while (2 * k <= sz){
         var j = 2 * k;
-        if (j < sz && getElement(heap, j) > getElement(heap, j + 1)) ++j; // 1 < 2
-        if (!(getElement(heap, k) > getElement(heap, j))) break; // 2 >= 1
+        if (j < sz && getElement(G, heap, j) > getElement(G, heap, j + 1)) ++j; // 1 < 2
+        if (!(getElement(G, heap, k) > getElement(G, heap, j))) break; // 2 >= 1
         swap(heap, k, j);
         k = j;
     }
 }// shiftDown
 
-function minheap_insert(heap, new_element) {
+function minheap_insert(G, heap, new_element) {
     heap.push(new_element);
-    shiftUp(heap);
+    shiftUp(G, heap);
 }
 
-function minheap_extract(heap) {
+function minheap_extract(G, heap) {
     var temp = heap[0];
     heap[0] = heap[heap.length - 1];
     heap.pop();
-    shiftDown(heap, 1);
+    shiftDown(G, heap, 1);
     return temp;
 }
 
@@ -89,17 +88,45 @@ function initSearchGraph() {
 
             // STENCIL: determine whether this graph node should be the start
             //   point for the search
-            if (ypos == 0 && xpos == 0){
+            if (ypos == q_init[1] && xpos == q_init[0]){
                 G[iind][jind].queued = true;
                 G[iind][jind].distance = 0;
                 G[iind][jind].priority = 1;
                 G[iind][jind].parent = G[iind][jind];
-                minheap_insert(visit_queue, [iind, jind, G[iind][jind].priority]);
+                minheap_insert(G, visit_queue, [iind, jind]);
             }
         }
     }
 }
 
+function calc_priority(G, curr, goal){
+    var xDiff =  G[goal[0]][goal[1]].x - G[curr[0]][curr[1]].x;
+    var yDiff =  G[goal[0]][goal[1]].y - G[curr[0]][curr[1]].y;
+    var euclidDist = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+
+    return euclidDist + G[curr[0]][curr[1]].distance;
+
+}
+//old check node
+// if (!G[temp_x][temp_y].visited && testCollision(G[temp_x][temp_y].x, G[temp_x][temp_y].y)){
+
+//     if(!(G[temp_x][temp_y].x == q_goal[0] && G[temp_x][temp_y].y == q_goal[1])){
+//         G[temp_x][temp_y].queued = true;
+//         minheap_insert(visit_queue, [temp_x, temp_y]);
+//         if (G[arr[0]][arr[1]].distance + 1 < G[temp_x][temp_y].distance){
+//             G[temp_x][temp_y].distance = G[arr[0]][arr[1]].distance + 1;
+//             G[temp_x][temp_y].parent = G[arr[0]][arr[1]];
+//             G[temp_x][temp_y].queued = true;
+//             G[temp_x][temp_y].priority = calc_distance(G, [temp_x, temp_y], q_goal);
+//             // minheap_insert(visit_queue, [arr[0], arr[1] - 1, G[arr[0]][arr[1] - 1].priority]);
+//         }
+//     }
+//     else{
+//         search_iterate = false;
+//         drawHighlightedPathGraph;
+//         return "succeeded";
+//     }
+// }
 
 function iterateGraphSearch() {
 
@@ -122,19 +149,91 @@ function iterateGraphSearch() {
     //   draw_2D_configuration - draws a square at a given location
 
     // while(!visit_queue.empty){
-        var arr = visit_queue.minheap_extract(visit_queue);
+        var arr = visit_queue.minheap_extract(G, visit_queue);
         G[arr[0]][arr[1]].visited = true;
         
-        if (!G[arr[0]][arr[1] - 1].visited && testCollision(G[arr[0]][arr[1] - 1].x, G[arr[0]][arr[1] - 1].y)){
-            G[arr[0]][arr[1] - 1].parent = G[arr[0]][arr[1]];
-            G[arr[0]][arr[1] - 1].visited = true;
+        //east
+        var temp_x = arr[0] + eps;
+        var temp_y = arr[1];
 
-            if(!G[arr[0]][arr[1] - 1] != goal){
-                G[arr[0]][arr[1] - 1].distance = G[arr[0]][arr[1]].distance + 1;
-                // G[arr[0]][arr[1] - 1].parent = G[arr[0]][arr[1]];
-                G[arr[0]][arr[1] - 1].queued = true;
-                G[arr[0]][arr[1] - 1].priority = calc_distance(G[arr[0]][arr[1] - 1], _____);
-                minheap_insert(visit_queue, [arr[0], arr[1] - 1, G[arr[0]][arr[1] - 1].priority]);
+        if (!G[temp_x][temp_y].visited && testCollision(G[temp_x][temp_y].x, G[temp_x][temp_y].y)){
+
+            if(!(G[temp_x][temp_y].x == q_goal[0] && G[temp_x][temp_y].y == q_goal[1])){
+                G[temp_x][temp_y].queued = true;
+                minheap_insert(visit_queue, [temp_x, temp_y]);
+                if (G[arr[0]][arr[1]].distance + 1 < G[temp_x][temp_y].distance){
+                    G[temp_x][temp_y].distance = G[arr[0]][arr[1]].distance + 1;
+                    G[temp_x][temp_y].parent = G[arr[0]][arr[1]];
+                    G[temp_x][temp_y].queued = true;
+                    G[temp_x][temp_y].priority = calc_distance(G, [temp_x, temp_y], q_goal);
+                    // minheap_insert(visit_queue, [arr[0], arr[1] - 1, G[arr[0]][arr[1] - 1].priority]);
+                }
+            }
+            else{
+                search_iterate = false;
+                drawHighlightedPathGraph;
+                return "succeeded";
+            }
+        }
+
+        //south
+        temp_x = arr[0];
+        temp_y = arr[1] - eps;
+        if (!G[temp_x][temp_y].visited && testCollision(G[temp_x][temp_y].x, G[temp_x][temp_y].y)){
+
+            if(!(G[temp_x][temp_y].x == q_goal[0] && G[temp_x][temp_y].y == q_goal[1])){
+                G[temp_x][temp_y].queued = true;
+                minheap_insert(visit_queue, [temp_x, temp_y]);
+                if (G[arr[0]][arr[1]].distance + 1 < G[temp_x][temp_y].distance){
+                    G[temp_x][temp_y].distance = G[arr[0]][arr[1]].distance + 1;
+                    G[temp_x][temp_y].parent = G[arr[0]][arr[1]];
+                    G[temp_x][temp_y].priority = calc_distance(G, [temp_x, temp_y], q_goal);
+                    // minheap_insert(visit_queue, [arr[0], arr[1] - 1, G[arr[0]][arr[1] - 1].priority]);
+                }
+            }
+            else{
+                search_iterate = false;
+                drawHighlightedPathGraph;
+                return "succeeded";
+            }
+        }
+
+        //west
+        temp_x = arr[0] - eps;
+        temp_y = arr[1];
+        if (!G[temp_x][temp_y].visited && testCollision(G[temp_x][temp_y].x, G[temp_x][temp_y].y)){
+
+            if(!(G[temp_x][temp_y].x == q_goal[0] && G[temp_x][temp_y].y == q_goal[1])){
+                G[temp_x][temp_y].queued = true;
+                minheap_insert(visit_queue, [temp_x, temp_y]);
+                if (G[arr[0]][arr[1]].distance + 1 < G[temp_x][temp_y].distance){
+                    G[temp_x][temp_y].distance = G[arr[0]][arr[1]].distance + 1;
+                    G[temp_x][temp_y].parent = G[arr[0]][arr[1]];
+                    G[temp_x][temp_y].priority = calc_distance(G, [temp_x, temp_y], q_goal);
+                    // minheap_insert(visit_queue, [arr[0], arr[1] - 1, G[arr[0]][arr[1] - 1].priority]);
+                }
+            }
+            else{
+                search_iterate = false;
+                drawHighlightedPathGraph;
+                return "succeeded";
+            }
+        }
+
+        //north 
+        temp_x = arr[0];
+        temp_y = arr[1] + eps;
+        if (!G[temp_x][temp_y].visited && testCollision(G[temp_x][temp_y].x, G[temp_x][temp_y].y)){
+
+            if(!(G[temp_x][temp_y].x == q_goal[0] && G[temp_x][temp_y].y == q_goal[1])){
+                G[temp_x][temp_y].queued = true;
+                minheap_insert(visit_queue, [temp_x, temp_y]);
+                if (G[arr[0]][arr[1]].distance + 1 < G[temp_x][temp_y].distance){
+                    G[temp_x][temp_y].distance = G[arr[0]][arr[1]].distance + 1;
+                    G[temp_x][temp_y].parent = G[arr[0]][arr[1]];
+                    G[temp_x][temp_y].priority = calc_distance(G, [temp_x, temp_y], q_goal);
+                    // minheap_insert(visit_queue, [arr[0], arr[1] - 1, G[arr[0]][arr[1] - 1].priority]);
+                }
             }
             else{
                 search_iterate = false;
