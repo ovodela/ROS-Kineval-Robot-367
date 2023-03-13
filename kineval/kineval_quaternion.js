611243cf9726c78907bafd49713576f533cd5388
ovodela <102893727+ovodela@users.noreply.github.com>
2023-03-12 20:31:25 -0400

    quaternion

--- kineval/kineval_quaternion.js
+++ kineval/kineval_quaternion.js
@@ -10,26 +10,69 @@
 
 // **** Function stencils are provided below, please uncomment and implement them ****//
 
-// kineval.quaternionFromAxisAngle = function quaternion_from_axisangle(axis,angle) {
-//     // returns quaternion q as dic, with q.a as real number, q.b as i component, q.c as j component, q.d as k component
-//     var q = {};
+kineval.quaternionFromAxisAngle = function quaternion_from_axisangle(axis,angle) {
+    // returns quaternion q as dic, with q.a as real number, q.b as i component, q.c as j component, q.d as k component
+    var q = {};
+    q.a = Math.cos(angle/2);
+    q.b = axis[0] * Math.sin(angle/2);
+    q.c = axis[1] * Math.sin(angle/2);
+    q.d = axis[2] * Math.sin(angle/2);
 
-// }
+    return q;
 
-// kineval.quaternionNormalize = function quaternion_normalize(q1) {
-//     // returns quaternion q as dic, with q.a as real number, q.b as i component, q.c as j component, q.d as k component
-//     var q = {};
+}
 
-// }
+kineval.quaternionNormalize = function quaternion_normalize(q1) {
+    // returns quaternion q as dic, with q.a as real number, q.b as i component, q.c as j component, q.d as k component
+    var q = {};
+    var mag = q1.a * q1.a + q1.b * q1.b + q1.c * q1.c + q1.d * q1.d;
 
-// kineval.quaternionMultiply = function quaternion_multiply(q1,q2) {
-//     // returns quaternion q as dic, with q.a as real number, q.b as i component, q.c as j component, q.d as k component
-//     var q = {};
+    mag = Math.sqrt(mag);
+
+    if (!(Math.abs(mag) < Number.EPSILON)){
+        q.a = q1.a / mag;
+        q.b = q1.b / mag;
+        q.c = q1.c / mag;
+        q.d = q1.d / mag;
+
+    }
+    else{
+        q.a = q1.a;
+        q.b = q1.b;
+        q.c = q1.c;
+        q.d = q1.d;   
+    }
     
+    return q;
 
-// }
+}
 
-// kineval.quaternionToRotationMatrix = function quaternion_to_rotation_matrix (q) {
-//     // returns 4-by-4 2D rotation matrix
+kineval.quaternionMultiply = function quaternion_multiply(q1,q2) {
+    // returns quaternion q as dic, with q.a as real number, q.b as i component, q.c as j component, q.d as k component
+    var q = {};
+    q.a = q1.a * q2.a - q1.b * q2.b - q1.c * q2.c - q1.d * q2.d;
+    q.b = q1.a * q2.b + q1.b * q2.a + q1.c * q2.d - q1.d * q2.c;
+    q.c = q1.a * q2.c - q1.b * q2.d + q1.c * q2.a + q1.d * q2.b;
+    q.d = q1.a * q2.d - q1.b * q2.c + q1.c * q2.b + q1.d * q2.a;
+    
+    return q;
 
-// }
+}
+
+kineval.quaternionToRotationMatrix = function quaternion_to_rotation_matrix (q) {
+    // returns 4-by-4 2D rotation matrix
+    var m = generate_identity();
+
+    m[0][0] = q.a * q.a + q.b * q.b - q.c * q.c - q.d * q.d;
+    m[0][1] = 2 * (q.b * q.c - q.a * q.d);
+    m[0][2] = 2 * (q.a * q.c + q.b * q.d);
+    m[1][0] = 2 * (q.b * q.c + q.a * q.d);
+    m[1][1] = q.a * q.a - q.b * q.b + q.c * q.c - q.d * q.d;
+    m[1][2] = 2 * (q.d * q.c - q.a * q.b);
+    m[2][0] = 2 * (q.b * q.d - q.a * q.c);
+    m[2][1] = 2 * (q.b * q.a + q.c * q.d);
+    m[2][2] = q.a * q.a - q.b * q.b - q.c * q.c + q.d * q.d;
+
+    return m;
+
+}
