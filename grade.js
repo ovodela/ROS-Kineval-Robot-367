688828dd0b26f48a19dcb99bce3b0f4c2b7ce79a
Liz Olson <lizolson@umich.edu>
2023-03-27 22:38:16 -0400

    Update grades at time: 27-03-2023-22-38-11

--- grade.js
+++ grade.js
@@ -1,4 +1,4 @@
-//CI grader generates report at 27-03-2023-20-25-39
+//CI grader generates report at 27-03-2023-22-25-46
 //Commit hash: 9653059
 grade = 
 {
@@ -140,14 +140,14 @@ grade =
         }
       },
       "FK_MatrixRoutines": {
-        "status": "PASS",
+        "status": "PENDING",
         "comments": [
-          "1 out of 1 test cases passed"
+          "0 out of 1 test cases passed"
         ],
         "test results": {
           "Test 0": {
-            "Status": "Succeed",
-            "Stderr": "b''"
+            "Status": "Failed",
+            "Stderr": "Error message too long, print partially: b\"/omkarv/FK_MatrixRoutines/test_0.js:727\\ntest_main()numeric = require('./numeric-1.2.6.js')\\n           ^^^^^^^\\n\\nSyntaxError: Unexpected identifier\\n    at createScript (vm.js:80:10)\\n    at Object.runInThisContext (vm.js:139:10)\\n    at Module._compile (module.js:616:28)\\n    at Object.Module._extensions..js (module.js:663:10)\\n    at Module.load (module.js:565:32)\\n    at tryModuleLoad (module.js:505:12)\\n    at Function.Modu"
           }
         }
       },
@@ -198,14 +198,14 @@ grade =
         ]
       },
       "FSMDance_Quaternion": {
-        "status": "PASS",
+        "status": "PENDING",
         "comments": [
-          "1 out of 1 test cases passed"
+          "0 out of 1 test cases passed"
         ],
         "test results": {
           "Test 0": {
-            "Status": "Succeed",
-            "Stderr": "b''"
+            "Status": "Failed",
+            "Stderr": "Error message too long, print partially: b\"/omkarv/FSMDance_Quaternion/test_0.js:369\\ntest_main()numeric = require('./numeric-1.2.6.js')\\n           ^^^^^^^\\n\\nSyntaxError: Unexpected identifier\\n    at createScript (vm.js:80:10)\\n    at Object.runInThisContext (vm.js:139:10)\\n    at Module._compile (module.js:616:28)\\n    at Object.Module._extensions..js (module.js:663:10)\\n    at Module.load (module.js:565:32)\\n    at tryModuleLoad (module.js:505:12)\\n    at Function.Mo"
           }
         }
       },
